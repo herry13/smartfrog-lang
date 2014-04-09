03ae37203da29f200e991b75a48a885c31410f0e
Herry <herry13@gmail.com>
2014-04-09 17:08:20 +0100

    fix minor bugs

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR="$(dirname "$0")"
-BIN="$BASEDIR/../sfConfig"
+BIN="$BASEDIR/../sfParser"
 
 echo "Running tests..."
-for FILE in "$BASEDIR/*"
+for file in $BASEDIR/*
 do
-	if [[ -e $FILE ]]; then
-		$BIN $FILE
+	if [[ -f $file && "${file##*.}" = "sf" ]]; then
+		echo -n "$file "
+		#result=$($BIN $file 1>/dev/null)
+		result=$($BIN $file 2>&1 1>/dev/null)
+		if [[ $result != "" ]]; then
+			echo "[Failed]"
+		else
+			echo "[OK]"
+		fi
 	fi
 done
 echo "...done!"
