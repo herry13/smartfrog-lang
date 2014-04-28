fd915b2c03adba93d100583d713d377e80921071
Herry <herry13@gmail.com>
2014-04-28 23:08:17 +0100

    include LISA tests into test script; add comments to figure4.sf

--- test/run.sh
+++ test/run.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 BASEDIR="$(dirname "$0")"
+LISADIR="$BASEDIR/LISA2014paper"
 BIN="$BASEDIR/../sfParser"
 
-echo "Running tests..."
-for file in $BASEDIR/*
-do
-	if [[ -f $file && "${file##*.}" = "sf" ]]; then
-		echo -n "$file "
-		#result=$($BIN $file 1>/dev/null)
-		result=$($BIN $file 2>&1 1>/dev/null)
+function test {
+	if [[ -f $1 && "${1##*.}" = "sf" ]]; then
+		echo -n "$1 "
+		result=$($BIN $1 2>&1 1>/dev/null)
 		if [[ $result != "" ]]; then
 			echo "[Failed]"
 		else
 			echo "[OK]"
 		fi
 	fi
+}
+
+echo "Running tests..."
+
+for file in $BASEDIR/*; do
+	test $file
 done
+
+for file in $LISADIR/*; do
+	test $file
+done
+
 echo "...done!"
