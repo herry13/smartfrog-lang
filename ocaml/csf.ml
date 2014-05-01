69d9f669948f4a4f4ba97adaf1d2ce1ffd864399
Herry <herry13@gmail.com>
2014-05-01 06:11:12 +0100

    add lexer buffer object

--- ocaml/csf.ml
+++ ocaml/csf.ml
@@ -2,11 +2,18 @@ open Array
 
 let help = "usage: csf <sf-file>\n\n"
 
+let buf file =
+	object
+		val ic = open_in file
+		method lex : (string -> int -> int) =
+			fun s n -> input ic s 0 n
+	end
+
 let compile file =
 	let ic = open_in file in
 	try
-		let lexbuf = Lexing.from_channel ic in
-		let result = Sfparser.sf Sflexer.token lexbuf in
+		let b = buf file in
+		let result = Sfparser.sf Sflexer.token (Lexing.from_function b#lex) in
 			print_string (Domain.yaml_of_store result);
             print_newline();
             flush stdout
