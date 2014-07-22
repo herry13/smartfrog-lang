96f42df5c7e76f1f4e4265788351ecb9a6b16b7f
Herry <herry13@gmail.com>
2014-07-22 04:48:45 +0100

    refactor codes

--- ocaml/csf.ml
+++ ocaml/csf.ml
@@ -1,22 +1,56 @@
+(*
+  csf.ml - main file
+  author: Herry (herry13@gmail.com)
+
+  usage: csf [option] <sf-file>
+
+  options:
+  -sf     print output in plain SF
+  -json   print output in JSON
+  -yaml   print output in YAML
+  -xml    print output in XML
+
+  changelog:
+  22.07.2014 - first released
+*)
+
 open Array
 
+(* help message *)
 let help = "usage: csf [option] <sf-file>" ^
-  "\n\nwhere [option] is:" ^
-  "\n  -sf     print output in plain SF" ^
-  "\n  -json   print output in JSON" ^
-  "\n  -yaml   print output in YAML" ^
-  "\n  -xml    print output in XML\n\n"
+           "\n\nwhere [option] is:" ^
+		   "\n  -sf     print output in plain SF" ^
+		   "\n  -json   print output in JSON" ^
+		   "\n  -yaml   print output in YAML" ^
+		   "\n  -xml    print output in XML\n\n"
 
-let compile opt file =
-  let result = Sf.parse file in
-  if opt = "-yaml" then print_string (Domain.yaml_of_store result)
-  else if opt = "-xml" then print_string (Domain.xml_of_store result)
-  else if opt = "-sf" then print_string (Domain.sf_of_store result)
-  else print_string (Domain.json_of_store result);
-  print_newline()
+(*
+	parse main SF file
+	@file - file to be parsed
+*)
+let parse_file file =
+	let dummy_lexbuf = Lexing.from_string "" in
+	let lexstack = Sfhelper.create file Sflexer.token in
+	try
+		Sfparser.sf (Sfhelper.get_token lexstack) dummy_lexbuf
+	with e -> Sfhelper.check_error e lexstack
 
+(*
+	parse file with given option
+	@opt - option
+	@file - file to be parsed	
+*)
+let parse_option_file opt file =
+	let result = parse_file file in
+	if opt = "-yaml" then print_string (Domain.yaml_of_store result)
+	else if opt = "-xml" then print_string (Domain.xml_of_store result)
+	else if opt = "-sf" then print_string (Domain.sf_of_store result)
+	else print_string (Domain.json_of_store result);
+	print_newline()
+
+(* main function *)
 let _ =
-  if (length Sys.argv) < 2 then print_string help
-  else if Sys.argv.(1) = "-h" || Sys.argv.(1) = "--help" then print_string help
-  else if (length Sys.argv) >= 3 then compile Sys.argv.(1) Sys.argv.(2)
-  else compile "" Sys.argv.(1)
+	if (length Sys.argv) < 2 then print_string help
+	else if Sys.argv.(1) = "-h" || Sys.argv.(1) = "--help" then print_string help
+	else if (length Sys.argv) >= 3 then parse_option_file Sys.argv.(1) Sys.argv.(2)
+	else parse_option_file "" Sys.argv.(1)
