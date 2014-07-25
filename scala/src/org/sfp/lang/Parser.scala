3dfc126cc9610b159b41a968bcd060434e3a7a5d
Herry <herry13@gmail.com>
2014-07-25 07:02:18 +0100

    [scala] enable -i option; add test/test1.sfp; add sfp script

--- scala/src/org/sfp/lang/Parser.scala
+++ scala/src/org/sfp/lang/Parser.scala
@@ -12,7 +12,7 @@ import org.sf.lang.Store
  * The main object of sfParser application.
  */
 object Parser extends Parser with App {
-  val help = """Usage: scala org.sf.lang.Parser [option] <sf-file>
+  val help = """Usage: scala org.sfp.lang.Parser [option] <sfp-file>
 
 where [option] is:
   -json     print output in JSON
@@ -21,14 +21,15 @@ where [option] is:
     
   if (args.length <= 0) Console.println(help)
   else {
-    /*try {
-      if (inJson(args)) println(parseFile(args.tail.head).toJson)
+    try {
+      if (isInclude(args)) println(parseIncludeFile(args.tail.head, org.sf.lang.Reference.empty, Store.empty))
+      else if (inJson(args)) println(parseFile(args.tail.head).toJson)
       else if (inYaml(args)) println(parseFile(args.tail.head).toYaml)
       else println(parseFile(args.head))
     } catch {
       case se: SemanticsException => System.err.println(se.msg)
       case e: Exception => System.err.println(e)
-    }*/
+    }
   }
     
   def inJson(args: Array[String]): Boolean =
@@ -36,6 +37,9 @@ where [option] is:
     
   def inYaml(args: Array[String]): Boolean =
     (args.length >= 2 && args.head.equals("-yaml"))
+    
+  def isInclude(args: Array[String]): Boolean =
+    (args.length >= 2 && args.head.equals("-i"))
 }
 
 class Parser extends JavaTokenParsers {
