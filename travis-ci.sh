10d42ac86bc25664ec1724da2c9a4994528ec194
Herry <herry13@gmail.com>
2014-08-02 15:58:55 +0100

    install cabal-install package

--- travis-ci.sh
+++ travis-ci.sh
@@ -40,7 +40,7 @@ cd ..
 #######################################
 
 # install Haskell from apt
-sudo apt-get install -qq ghc
+sudo apt-get install -qq ghc cabal-install
 cabal install Parsec MissingH Safe QuickCheck
 cd haskell
 make
