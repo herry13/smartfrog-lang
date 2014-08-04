19a905ccd33f5552cf45e3f4d576a244aee05a4c
Herry <herry13@gmail.com>
2014-08-04 17:42:08 +0100

    [ocaml] use function resolve to dereference data & link references

--- ocaml/sftype.ml
+++ ocaml/sftype.ml
@@ -30,7 +30,7 @@ let string_of_env e =
 	in
 	str_of_env e ""
 
-(** alias functions from module Sfdomain **)
+(** alias of functions from module Sfdomain **)
 let ref_plus_ref = Sfdomain.ref_plus_ref;;
 let ref_minus_ref = Sfdomain.ref_minus_ref;;
 let prefix = Sfdomain.prefix;;
@@ -49,10 +49,10 @@ let rec type_of e r =
 	| (vr, vt) :: tail -> if r = vr then Type vt else type_of tail r
 
 (** return true if variable 'r' is in environment 'e', otherwise false *)
-let rec dom e r =
+let rec domain e r =
 	match e with
 	| []               -> false
-	| (vr, vt) :: tail -> if r = vr then true else (dom e r)
+	| (vr, vt) :: tail -> if r = vr then true else (domain e r)
 
 (* return true if type 't' is available in environment 'e', otherwise false *)
 let rec has_type e t =
@@ -64,14 +64,14 @@ let rec has_type e t =
 	| TBasic TNum               -> true                    (* (Type Num)    *)
 	| TBasic TStr               -> true                    (* (Type Str)    *)
 	| TBasic TNull              -> true                    (* (Type Null)   *)
-	| TBasic TObject               -> true                    (* (Type Object) *)
-	| TBasic TAction               -> true                    (* (Type Action) *)
-	| TBasic TGlobal              -> true                    (* (Type Global) *)
+	| TBasic TObject            -> true                    (* (Type Object) *)
+	| TBasic TAction            -> true                    (* (Type Action) *)
+	| TBasic TGlobal            -> true                    (* (Type Global) *)
 	| TBasic (TSchema (sid, _)) ->
 		match e with
-		| []                                                    -> false
+		| []                                        -> false
 		| (_, (TBasic (TSchema (side, _)))) :: tail -> if sid = side then true else has_type tail t  (* (Type Schema) *)
-		| (_, _) :: tail                                        -> has_type tail t
+		| (_, _) :: tail                            -> has_type tail t
 
 (* bind type 't' to variable 'r' into environment 'e' *)
 let bind e r t =
@@ -101,70 +101,22 @@ let rec (<:) t1 t2 =
 	if t1 = t2 then true                                                  (* (Reflex)         *)
 	else
 		match t1, t2 with
-		| TBasic (TSchema _), TBasic TObject    -> true                      (* (Object Subtype) *)
+		| TBasic (TSchema _), TBasic TObject -> true                      (* (Object Subtype) *)
 		| TBasic (TSchema (sid1, super1)), _ -> TBasic super1 <: t2       (* (Trans)          *)
 		| TVec tv1, TVec tv2                 -> tv1 <: tv2                (* (Vec Subtype)    *)
 		| TRef tr1, TRef tr2                 -> TBasic tr1 <: TBasic tr2  (* (Ref Subtype)    *)
 		| TBasic TNull, TRef _               -> true                      (* (Ref Null)       *)
 		| _, _ -> false
 
-
-(*******************************************************************
- * type inference rules
- *******************************************************************)
-
-let sfBoolean b = TBasic TBool
-
-let sfNumber n = TBasic TNum
-
-let sfString s = TBasic TStr
-
-let sfNull = TBasic TNull
-
-let sfReference r = r
-
-let sfDataReference dr =
-	fun e ->
-		match type_of e (sfReference dr) with
-		| Type (TBasic t) -> TRef t
-		| Type (TRef t)   -> TRef t
-		| Type (TVec _)   -> failure 101 "dereference of data reference is a vector"
-		| Type TUndefined -> failure 102 "dereference of data reference is TUndefined"
-		| Undefined       -> failure 103 "dereference of data reference is undefined"
-
-let sfLinkReference lr =
-	fun e ->
-		match type_of e (sfReference lr) with
-		| Undefined -> failure 104 "dereference of link reference is undefined"
-		| Type t    -> t
-
-let rec sfVector vec =
-	fun e ->
-		let rec eval v =
-			match v with
-			| [] -> TUndefined
-			| head :: [] -> sfBasicValue head e
-			| head :: tail ->
-				let t_head = sfBasicValue head e in
-				if t_head = eval tail then t_head
-				else failure 105 "types of vector elements are different"
-		in
-		TVec (eval vec)
-
-and sfBasicValue bv =
-	fun e ->
-		match bv with
-		| Boolean b  -> sfBoolean b
-		| Number n   -> sfNumber n
-		| String s   -> sfString s
-		| Null       -> sfNull
-		| Vector vec -> sfVector vec e
-		| DR dr      -> sfDataReference dr e
-
+(**
+ * @param e  type environment
+ * @param ns namespace where the reference will be resolved
+ * @param r  reference to be resolved
+ *)
 let rec resolve e ns r =
 	match r with
 	| "ROOT"   :: rs -> ([], type_of e (simplify rs))
-	| "PARENT" :: rs -> if ns = [] then failure 10 "PARENT of root namespace is impossible"
+	| "PARENT" :: rs -> if ns = [] then failure 2 "PARENT of root namespace is impossible"
 	                    else (prefix ns, type_of e (simplify (ref_plus_ref (prefix ns) rs)))
 	| "THIS"   :: rs -> (ns, type_of e (simplify (ref_plus_ref ns rs)))
 	| _              ->
@@ -178,13 +130,79 @@ let rec resolve e ns r =
 let inherit_env e ns proto r =
 	let get_proto =
 		match resolve e ns proto with
-		| _, Undefined          -> failure 12 ("prototype is not found: " ^ (string_of_ref proto))
+		| _, Undefined             -> failure 3 ("prototype is not found: " ^ (string_of_ref proto))
 		| nsx, Type TBasic TObject -> ref_plus_ref nsx proto
-		| _, Type t             -> failure 13 ("invalid prototype: " ^ (string_of_type t))
+		| _, Type t                -> failure 4 ("invalid prototype: " ^ (string_of_type t))
 	in
 	let ref_proto = get_proto in
 	let e_proto = env_of_ref e ref_proto true in
 	List.fold_left (fun ep (rep, tep) -> ((ref_plus_ref r rep), tep) :: ep) e e_proto
+
+let assign e r t t_value =
+	match type_of e r with
+	| Undefined  ->
+		if t = TUndefined then bind e r t_value      (* (Assign1) *)
+		else if t_value <: t then bind e r t         (* (Assign3) *)
+		else failure 5 "not satisfy rule (Assign3)"
+	| Type t_var ->
+		if t = TUndefined then
+			if t_value <: t_var then e                           (* (Assign2) *)
+			else failure 6 "not satisfy rule (Assign2)"
+		else if (t_value <: t) && (t <: t_var) then e            (* (Assign4) *)
+		else failure 7 "not satisfy rule (Assign2) & (Assign4)"
+
+
+(*******************************************************************
+ * type inference rules
+ *******************************************************************)
+
+let sfBoolean b = TBasic TBool  (* (Bool) *)
+
+let sfNumber n = TBasic TNum    (* (Num)  *)
+
+let sfString s = TBasic TStr    (* (Str)  *)
+
+let sfNull = TBasic TNull       (* (Null) *)
+
+let sfReference r = r
+
+let sfDataReference dr =  (* (Deref Data) *)
+	fun e ns ->
+		match resolve e ns (sfReference dr) with
+		| _, Type (TBasic t) -> TRef t
+		| _, Type (TRef t)   -> TRef t
+		| _, Type (TVec _)   -> failure 101 "dereference of data reference is a vector"
+		| _, Type TUndefined -> failure 102 "dereference of data reference is TUndefined"
+		| _, Undefined       -> failure 103 "dereference of data reference is undefined"
+
+let sfLinkReference lr =  (* (Deref Link) *)
+	fun e ns ->
+		match resolve e ns (sfReference lr) with
+		| _, Undefined -> failure 104 "dereference of link reference is undefined"
+		| _, Type t    -> t
+
+let rec sfVector vec =
+	fun e ns ->
+		let rec eval v =  (* (Vec) *)
+			match v with
+			| [] -> TUndefined
+			| head :: []   -> sfBasicValue head e ns
+			| head :: tail ->
+				let t_head = sfBasicValue head e ns in
+				if t_head = eval tail then t_head
+				else failure 105 "types of vector elements are different"
+		in
+		TVec (eval vec)
+
+and sfBasicValue bv =
+	fun e ns ->
+		match bv with
+		| Boolean b  -> sfBoolean b
+		| Number n   -> sfNumber n
+		| String s   -> sfString s
+		| Null       -> sfNull
+		| Vector vec -> sfVector vec e ns
+		| DR dr      -> sfDataReference dr e ns
 
 let rec sfPrototype proto =
 	fun ns r e ->
@@ -194,14 +212,19 @@ let rec sfPrototype proto =
 		| EmptyPrototype -> e
 
 and sfValue v =
-	fun ns r e ->
+	(**
+	 * @param ns namespace
+	 * @param t  predefined type
+	 * @param e  type environment
+	 *)
+	fun ns r t e ->
 		match v with
-		| BV bv   -> bind e r (sfBasicValue bv e)
-		| LR link -> bind e r (sfLinkReference link e)
+		| BV bv   -> assign e r t (sfBasicValue bv e ns)
+		| LR link -> assign e r t (sfLinkReference link e ns)
 		| P proto -> sfPrototype proto ns r (bind e r (TBasic TObject))
 
 and sfAssignment (r, t, v) =
-	fun ns e -> sfValue v ns (ref_plus_ref ns r) e
+	fun ns e -> sfValue v ns (ref_plus_ref ns r) t e
 
 and sfBlock block =
 	fun ns e ->
