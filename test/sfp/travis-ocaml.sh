de2ea41c20137dbd53468f4a6a7968bae06e614a
Herry <herry13@gmail.com>
2014-08-07 00:31:23 +0100

    [test] test int & float in primitive.sfp; test AST,JSON,YAML,Type outputs

--- test/sfp/travis-ocaml.sh
+++ test/sfp/travis-ocaml.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 BASEDIR="$(dirname "$0")"
-OPT="-type"
-BIN="$BASEDIR/../../ocaml/csfp $OPT"
+OPT_TYPE="-type"
+OPT_JSON="-json"
+OPT_YAML="-yaml"
+OPT_AST="-ast"
+BIN="$BASEDIR/../../ocaml/csfp"
 EXT="sfp"
 
 function test {
 	if [[ -f $1 && "${1##*.}" = $EXT ]]; then
-		result=$($BIN $1 2>&1 1>/dev/null)
+		result=$($BIN $2 $1 2>&1 1>/dev/null)
 		if [[ $result != "" ]]; then
 			echo "$1 [Failed]"
 		else
@@ -19,6 +22,13 @@ function test {
 echo "=== running tests ==="
 filelist="good-test-files.txt"
 for file in $(cat $filelist); do
-	test $file
+	# AST: -ast
+	test $file $OPT_AST
+	# type: -type
+	test $file $OPT_TYPE
+	# JSON: -json
+	test $file $OPT_JSON
+	# YAML: -yaml
+	test $file $OPT_YAML
 done
 echo "=== done ==="
