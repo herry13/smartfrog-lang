fa1d27ea5624d51f4aee5a3bc35250ee8ec3c487
Herry <herry13@gmail.com>
2014-08-08 15:01:48 +0100

    [ocaml] combine typetable of init & goal spec

--- ocaml/csf.ml
+++ ocaml/csf.ml
@@ -2,9 +2,8 @@
   csf.ml - main file
   author: Herry (herry13@gmail.com)
 
-  usage: csf [option] <sf-file>
+  usage: csfp [option] <sf-file>
   where [option] is:
-  -sf     print store in plain SF
   -json   print store in JSON (default)
   -yaml   print store in YAML
   -ast    print abstract syntax tree
@@ -16,15 +15,31 @@
 open Array
 
 (* help message *)
-let help = "usage: csf [option] <sf-file>" ^
+let help = "usage: csfp [option] <sfp-file> [sfp-goal-file]" ^
            "\n\nwhere [option] is:" ^
            "\n  -ast    print abstract syntax tree" ^
            "\n  -type   evaluate types and print the type environment" ^
 		   "\n  -json   evaluate values and print store in JSON (default)" ^
 		   "\n  -yaml   evaluate values and print store in YAML" ^
 		   "\n  -fs     evaluate values and print flat store" ^
-		   "\n  -tt     generate and print type table" ^
+		   "\n  -fdr    generate Finite Domain Representation (FDR)" ^
+           "\n          [sfp-goal-file] must be provided" ^
            "\n\n"
+
+let ast_of_file file =
+	let dummy_lexbuf = Lexing.from_string "" in
+	let lexstack = Sfhelper.create file Sflexer.token in
+	try
+		Sfparser.sfp (Sfhelper.get_token lexstack) dummy_lexbuf
+	with e ->
+		try
+			Sfhelper.check_error e lexstack
+		with
+		| Sfhelper.ParseError (fname, lnum, lpos, token) ->
+			prerr_string ("--- Parse error ---\nfile:   " ^ fname ^ "\nline:   " ^ (string_of_int lnum) ^
+			              "\ncolumn: " ^ (string_of_int lpos) ^ "\ntoken:  '" ^ token ^ "'\n\n");
+			exit 1
+		| e -> raise e
 
 (**
  * Parse main SF file.
@@ -33,35 +48,20 @@ let help = "usage: csf [option] <sf-file>" ^
  * @param opt  an option       
  * @return Sfsyntax.sf an SF abstract syntax tree
  *)
-let rec parse_file opt file =
-	let dummy_lexbuf = Lexing.from_string "" in
-	let lexstack = Sfhelper.create file Sflexer.token in
-	let ast =
-		try
-			Sfparser.sfp (Sfhelper.get_token lexstack) dummy_lexbuf
-		with e ->
-			try
-				Sfhelper.check_error e lexstack
-			with
-			| Sfhelper.ParseError (fname, lnum, lpos, token) ->
-				prerr_string ("--- Parse error ---\nfile:   " ^ fname ^ "\nline:   " ^ (string_of_int lnum) ^
-				              "\ncolumn: " ^ (string_of_int lpos) ^ "\ntoken:  '" ^ token ^ "'\n\n");
-				exit 1
-			| e -> raise e
-	in
+let rec parse_file opt file1 file2 =
 	let str =
 		try
 			match opt with
-			| "-ast"  -> Sfsyntax.string_of_sfp ast
+			| "-ast"  -> Sfsyntax.string_of_sfp (ast_of_file file1)
 			| "-type" ->
-				let env = Sftype.sfpSpecification ast in
+				let env = Sftype.sfpSpecification (ast_of_file file1) in
 				Sftype.string_of_env env
 			| "-fs"   ->
-				let fs = Sastranslator.create_flat_store (eval_value ast) in
+				let fs = Sastranslator.normalise (eval_value (ast_of_file file1)) in
 				Sastranslator.string_of_flat_store fs
-			| "-sas"  -> Sastranslator.sas ast
-			| "-yaml" -> Sfdomainhelper.yaml_of_store (eval_value ast)
-			| _       -> Sfdomainhelper.json_of_store (eval_value ast)
+			| "-fdr"  -> Sastranslator.fdr (ast_of_file file1) (ast_of_file file2)
+			| "-yaml" -> Sfdomainhelper.yaml_of_store (eval_value (ast_of_file file1))
+			| _       -> Sfdomainhelper.json_of_store (eval_value (ast_of_file file1))
 		with
 		| Sftype.TypeError (code, msg) -> prerr_string (msg ^ "\n"); exit code
 		| Sfdomain.SfError (code, msg) -> prerr_string (msg ^ "\n"); exit code
@@ -74,7 +74,11 @@ and eval_value ast = Sfvaluation.sfpSpecification ast
  * main function
  *)
 let _ =
-	if (length Sys.argv) < 2 || (length Sys.argv) > 3 then print_string help
-	else if Sys.argv.(1) = "-h" || Sys.argv.(1) = "--help" then print_string help
-	else if (length Sys.argv) = 3 then parse_file Sys.argv.(1) Sys.argv.(2)
-	else parse_file "" Sys.argv.(1)
+	let l = length Sys.argv in
+	if l < 2 then print_string help
+	else
+		match Sys.argv.(1) with
+		| "-fdr" when l = 4 -> parse_file "-fdr" Sys.argv.(2) Sys.argv.(3)
+		| _      when l = 3 -> parse_file Sys.argv.(1) Sys.argv.(2) ""
+		| _      when l = 2 -> parse_file "" Sys.argv.(1) ""
+		| _                 -> print_string help
--- ocaml/sastranslator.ml
+++ ocaml/sastranslator.ml
@@ -27,7 +27,7 @@ module FlatStore = Map.Make
  * @param s store to be flatten
  * @return a flat_store
  *)
-let create_flat_store s =
+let normalise s =
 	let rec visit s ns fs =
 		match s with
 		| []              -> fs
@@ -80,43 +80,87 @@ module TypeTable = Map.Make
 		end
 	)
 
-let create_type_table e fs = (* e: type-environment, fs: flat-store *)
-	let t_obj = (Sfsyntax.TBasic Sfsyntax.TObject) in
-	let values_of_type t table =
-		if TypeTable.mem t table then TypeTable.find t table else SetValue.empty
+let t_bool = Sfsyntax.TBasic Sfsyntax.TBool
+let t_num = Sfsyntax.TBasic Sfsyntax.TNum
+let t_str = Sfsyntax.TBasic Sfsyntax.TStr
+let t_obj = Sfsyntax.TBasic Sfsyntax.TObject
+let t_action = Sfsyntax.TBasic Sfsyntax.TAction
+let t_global = Sfsyntax.TBasic Sfsyntax.TGlobal
+
+let values_of_type t typetable =
+	if TypeTable.mem t typetable then TypeTable.find t typetable else SetValue.empty
+
+let add_value_of_type t v typetable = 
+	TypeTable.add t (SetValue.add v (values_of_type t typetable)) typetable
+
+(** group action effects' values based on their type **)
+let add_action_values e typetable =
+	let actions = values_of_type t_action typetable in
+	let add_effect_values =
+		List.fold_left (
+			fun acc (r, v) ->
+				match v with
+				| Boolean _ -> add_value_of_type t_bool (Basic v) acc
+				| Number  _ -> add_value_of_type t_num (Basic v) acc
+				| String  _ -> add_value_of_type t_str (Basic v) acc
+				| Vector  _ -> error 503 (* TODO *)
+				| _         -> acc
+		) 
 	in
-	let add_value t v table = 
-		TypeTable.add t (SetValue.add v (values_of_type t table)) table
-	in
-	let rec add_object t v t_next table =
-		let table1 = add_value (Sfsyntax.TBasic t) v table in
-		let table2 = add_value (Sfsyntax.TRef t) v table1 in
+	SetValue.fold (
+		fun v typetable ->
+			match v with
+			| Action (n, ps, c, pre, eff) -> add_effect_values typetable eff
+			| _ -> typetable
+	) actions typetable
+
+(** group values based on their type *)
+let create_type_table env_0 fs_0 env_g fs_g = (* e: type-environment, fs: flat-store *)
+	let null = Basic Null in
+	let rec add_object t v t_next typetable =
+		let typetable1 = add_value_of_type (Sfsyntax.TBasic t) v typetable in
+		let typetable2 = add_value_of_type (Sfsyntax.TRef t) v typetable1 in
+		let typetable3 = add_value_of_type (Sfsyntax.TRef t) null typetable2 in
 		match t_next with
-		| Sfsyntax.TObject -> add_value t_obj v table2
-		| Sfsyntax.TSchema (sid, super) -> add_object t_next v super table2
+		| Sfsyntax.TObject -> add_value_of_type t_obj v typetable3
+		| Sfsyntax.TSchema (sid, super) -> add_object t_next v super typetable3
 		| _ -> error 501 (* super-type must be another schema or an object *)
 	in
-	FlatStore.fold (
-		fun r v table ->
-			match Sftype.type_of e r with
-			| Sftype.Undefined -> error 502
-			| Sftype.Type Sfsyntax.TBasic Sfsyntax.TSchema (sid, super) ->
-				add_object (Sfsyntax.TSchema (sid, super)) (Basic (Ref r)) super table
-			| Sftype.Type t -> add_value t v table
-	) fs TypeTable.empty 
+	let add_store_values env =
+		FlatStore.fold (
+			fun r v typetable ->
+				match Sftype.type_of env r with
+				| Sftype.Undefined -> error 502
+				| Sftype.Type Sfsyntax.TBasic Sfsyntax.TSchema (sid, super) ->
+					add_object (Sfsyntax.TSchema (sid, super)) (Basic (Ref r)) super typetable
+				| Sftype.Type t -> add_value_of_type t v typetable
+		)
+	in
+	let typetable00 = add_store_values env_0 fs_0 TypeTable.empty in
+	let typetable01 = add_action_values env_0 typetable00 in
+	let typetable10 = add_store_values env_g fs_g typetable01 in
+	add_action_values env_g typetable10
 
 let string_of_type_table tt =
 	TypeTable.fold (
 		fun t v acc -> acc ^ (Sfsyntax.string_of_type t) ^ ": " ^ (string_of_set_value v) ^ "\n"
 	) tt ""
 
-let sas ast =
+(*******************************************************************
+ * interface functions for translating SFP to FDR
+ *******************************************************************)
+
+let eval_ast ast = (Sftype.sfpSpecification ast, Sfvaluation.sfpSpecification ast)
+
+let fdr ast_0 ast_g =
 	(* type system *)
-	let e = Sftype.sfpSpecification ast in
-	let s = Sfvaluation.sfpSpecification ast in
-	let fs = create_flat_store s in
-	let tt = create_type_table e fs in
-	"=== flat-store ===\n" ^ (string_of_flat_store fs) ^
+	let (env_0, store_0) = eval_ast ast_0 in
+	let (env_g, store_g) = eval_ast ast_g in
+	let fs_0 = normalise store_0 in
+	let fs_g = normalise store_g in
+	let tt = create_type_table env_0 fs_0 env_g fs_g in
+	"=== flat-store current ===\n" ^ (string_of_flat_store fs_0) ^
+	"=== flat-store desired ===\n" ^ (string_of_flat_store fs_g) ^
 	"=== type table ===\n" ^ (string_of_type_table tt)
 
 
